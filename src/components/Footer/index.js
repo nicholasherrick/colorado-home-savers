305d85bddb058210c0ae4248681d4c011a06428f
Nicholas Herrick <nth.herrick@gmail.com>
2020-04-23 01:12:45 -0600

    fixed footer height and content wrap padding, added margin bottom to cards on home page

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,8 +4,10 @@ import './style.css';
 const Footer = () => {
   return (
     <div>
-      <div className='footer text-center text-white mt-4 p-4'>
-        <h3>Copyright &copy; 2020 Snow Lion, LLC. All rights reserved</h3>
+      <div className='footer text-center text-white'>
+        <h3 className='mt-3'>
+          Copyright &copy; 2020 Snow Lion, LLC. All rights reserved
+        </h3>
       </div>
     </div>
   );
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -33,7 +33,7 @@ const Home = () => {
       </div>
       <div className='container'>
         <div className='row'>
-          <div className='col-md-5'>
+          <div className='col-md-5 mb-3'>
             <ContactCard
               image={Jeff}
               cardTitle='Jeffrey Raymond Herrick'
@@ -42,7 +42,7 @@ const Home = () => {
               secondButton='About Me'
             />
           </div>
-          <div className='col-md-7'>
+          <div className='col-md-7 mb-3'>
             <ContactCardLarge
               cardTitle='The Foreclosure Timeline'
               text='Many people do not realize that there is a lot of time in the Colorado foreclosure process. First you miss at least three mortgage payments, maybe more, before your mortgage lender takes action. Then they have to file for foreclosure with the Public Trustee of the County your property resides in. This is called a Notice of Election and Demand (NED). They have to publish their intent to foreclose in several publications. This process can take several weeks. Once this process is done, the Public Trustee sets an auction or sale date which is anywhere from 110 to 130 days out. This auction, or sale date, is the deadline to save your property by noon the day before the date of the auction. As you see, once you stop paying your mortgage, you have at least six months, maybe much longer, to work to save your property before it can be taken away from you. I had a client once where the foreclosure process took more than two years. Unfortunately for him, he moved his family out of the house, against my recommendation, as soon as he stopped paying his mortgage and began renting. He said he wanted to get settled somewhere so he didn’t have to uproot his kids from schools, friends, etc. He could have lived in his home for more than two years without paying a dime in mortgage or rent payments, so it is important to NOT move out of your house. Let the process unfold until you are sure of the exact deadline. I have been successful in many cases extending the auction date out further and further, giving my clients even more time to avoid foreclosure.'
